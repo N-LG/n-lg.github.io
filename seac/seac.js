271a9688293a05dc7f763b2537ffba0658cbea72
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2024-08-10 23:22:04 +0200

    améliorations portofilo

--- seac/seac.js
+++ seac/seac.js
@@ -5,23 +5,24 @@ function initpage(){
 var elements = document.getElementsByClassName('petit');
 for (var i = 0; i < elements.length; i++) {
   elements[i].setAttribute('onmouseover', 'up(this)');
-  elements[i].setAttribute('onmouseout', 'down(this)');
 }
+up(elements[0])
 }
 
+
 function up(e){
-e.style.boxShadow = "2px 2px 2px #303030";
-e.style.position = "relative";
-e.style.top = "-1px";
-e.style.left = "-1px";
-e.style.transform  = 'rotate(10deg)';
+var elements = document.getElementsByClassName('petit');
+for (var i = 0; i < elements.length; i++) {
+  elements[i].setAttribute('onmouseover', 'up(this)');
+  elements[i].style.width = "100px";
+  elements[i].style.transform  = 'skewY(20deg)';
+}
+
+
+e.style.width = "200px";
+e.style.transform  = 'none';
 document.getElementById("affichage").src = e.src;
 document.getElementById("affichage").title = e.title;
 document.getElementById("legende").innerHTML = e.title;
 }
 
-function down(e){
-e.style.boxShadow = "0px 0px";
-e.style.position = "static";
-e.style.transform  = 'rotate(0deg)';
-}
